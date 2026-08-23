f42dbffd2aa6d945e276bfcbd7dd14da846be12c
Jonatha Targino <j.oliveiratargino@hotmail.com>
2026-08-23 16:11:07 -0300

    feat: add dlq, s3 object created event notification to sqs

--- backend/lib/video-transcriptor-cloudformation-stack.ts
+++ backend/lib/video-transcriptor-cloudformation-stack.ts
@@ -3,16 +3,40 @@ import { Construct } from "constructs";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import * as path from "node:path";
 import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3Notifications from "aws-cdk-lib/aws-s3-notifications";
 import { SqsEventSource } from "aws-cdk-lib/aws-lambda-event-sources";
 
 export class VideoTranscriptorCloudformationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const videoTranscriptorBucket = new s3.Bucket(this, "VideosToTranscript", {
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+
+    const videoTranscriptorDeadLetterQueue = new sqs.Queue(
+      this,
+      "VideoTranscriptorDeadLetterQueue",
+      { retentionPeriod: cdk.Duration.days(14) },
+    );
+
     const videoTranscriptorQueue = new sqs.Queue(
       this,
       "VideoTranscriptorQueue",
-      {},
+      {
+        deadLetterQueue: {
+          queue: videoTranscriptorDeadLetterQueue,
+          maxReceiveCount: 3,
+        },
+      },
+    );
+
+    videoTranscriptorBucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new s3Notifications.SqsDestination(videoTranscriptorQueue),
+      { suffix: ".mp4" },
     );
 
     const videoTranscriptorLambdaFunction = new NodejsFunction(
@@ -30,5 +54,7 @@ export class VideoTranscriptorCloudformationStack extends cdk.Stack {
     videoTranscriptorLambdaFunction.addEventSource(
       new SqsEventSource(videoTranscriptorQueue),
     );
+
+    videoTranscriptorBucket.grantRead(videoTranscriptorLambdaFunction);
   }
 }
